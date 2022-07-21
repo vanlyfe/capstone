c5764bf1f290e77f738a135535aada52f9c734cf
Joram Bosire <jbosire@jbosire-ltm73m6.internal.salesforce.com>
2022-07-21 10:59:14 -0700

    Added listings table sql

--- Backend/vanlyfe-schema.sql
+++ Backend/vanlyfe-schema.sql
@@ -16,7 +16,43 @@ CREATE TABLE users(
 
 );
 
+CREATE TABLE listings(
 
+    id                  SERIAL PRIMARY KEY,
+    user_id             INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    price               INTEGER NOT NULL,
+    location            TEXT NOT NULL,
+    max_accomodation    INTEGER NOT NULL,
+    model               TEXT,
+    description         TEXT,
+    image_url           TEXT NOT NULL,
+    image_url2          TEXT,
+    image_url3          TEXT,
+    image_url4          TEXT,
+    image_url5          TEXT,
+    createdAt           TIMESTAMP NOT NULL DEFAULT NOW(),
+    updatedAt           TIMESTAMP NOT NULL DEFAULT NOW(),
+    fees                FLOAT
+
+);
+
+CREATE TABLE  orders(
+    id                  SERIAL PRIMARY KEY,
+    user_id             INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    createdAt           TIMESTAMP NOT NULL DEFAULT NOW(),
+    updatedAt           TIMESTAMP NOT NULL DEFAULT NOW(),
+    fees                FLOAT,
+    taxes               FLOAT NOT NULL,
+    total               FLOAT NOT NULL,
+    guests              INTEGER NOT NULL,
+    dates               TEXT NOT NULL
+   
+
+
+
+);
 -- Ratings
 -- phone number
 -- payment method
