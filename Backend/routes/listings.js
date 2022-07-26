fdea025ffb6d140f66779578e78ed081fac59a85
Joram Bosire <jbosire@jbosire-ltm73m6.internal.salesforce.com>
2022-07-26 10:50:12 -0700

    Added GET request skeletons

--- Backend/routes/listings.js
+++ Backend/routes/listings.js
@@ -4,6 +4,29 @@ const security = require("../middleware/security")
 const permissions = require("../middleware/permissions")
 const router = express.Router();
 
+router.get("/", async (req, res, next) => {
+    try{
+
+    } catch(error){
+        next(error)
+    }
+})
+
+router.get("/user/:userId", security.requireAuthenticatedUser, async (req, res, next) => {
+    try{
+
+    } catch(error){
+        next(error)
+    }
+})
+
+router.get("/:listingId", security.requireAuthenticatedUser,  async (req, res, next) => {
+    try{
+
+    } catch(error){
+        next(error)
+    }
+})
 
 
 
--- Backend/routes/reviews.js
+++ Backend/routes/reviews.js
@@ -4,6 +4,13 @@ const security = require("../middleware/security")
 const permissions = require("../middleware/permissions")
 const router = express.Router();
 
+router.get("/:listingId", async (req, res, next) => {
+    try{
+
+    } catch(error){
+        next(error)
+    }
+})
 
 
 
