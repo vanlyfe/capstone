ace7f789be9e821ec5fd7c2cbc7f96f9bcd42612
Joram Bosire <jbosire@jbosire-ltm73m6.internal.salesforce.com>
2022-07-26 14:05:16 -0700

    Added GET endpoints for listings

--- Backend/models/listing.js
+++ Backend/models/listing.js
@@ -3,6 +3,47 @@ const { BadRequestError } = require("../utils/errors");
 
 
 class Listing{
+    static async getListings(){
+        const result = await db.query(`
+        SELECT * FROM listings;
+        `
+         )
+
+        const res = result.rows
+
+         return res;
+    }
+
+    static async getListingById(id){
+
+        const result = await db.query(
+            `
+            SELECT * FROM listings
+            WHERE id = $1;
+            `, [id]
+        )
+
+        const res = result.rows[0]
+
+
+        return res;
+        
+    }
+
+
+    static async getUserListings(userId){
+        const result = await db.query(
+            `
+            SELECT * FROM listings
+            WHERE user_id = $1;
+            `, [userId]
+        )
+
+        const res = result.rows
+        return res;
+    
+    }
+
     
 }
 
--- Backend/routes/listings.js
+++ Backend/routes/listings.js
@@ -7,21 +7,40 @@ const router = express.Router();
 router.get("/", async (req, res, next) => {
     try{
 
+        var listings = await Listing.getListings();
+
+        return res.status(200).json({listings : listings})
+
     } catch(error){
         next(error)
     }
 })
+
+
+
 
 router.get("/user/:userId", security.requireAuthenticatedUser, async (req, res, next) => {
     try{
+        var userId = req.params.userId
+        var listings = await Listing.getUserListings(userId)
+       
+        return res.status(200).json({listings: listings})
 
     } catch(error){
         next(error)
     }
 })
 
+
+
 router.get("/:listingId", security.requireAuthenticatedUser,  async (req, res, next) => {
     try{
+
+        
+        var id = req.params.listingId
+
+        var listing = await Listing.getListingById(id)
+        return res.status(200).json({listing: listing})
 
     } catch(error){
         next(error)
