fec1d98eeb299b967f76f851adbe9cc26f0ba01f
Edilawit Tsehay <edilawittsehay@gmail.com>
2022-07-26 10:27:01 -0700

    changes to theme

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,4 +1,3 @@
-
 import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
@@ -14,10 +13,10 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import logo from "../assets/Logo1.png"
-import {Navigate, useNavigate} from "react-router-dom"
-import {useState} from "react"
+// import { createTheme, ThemeProvider } from "@mui/material/styles";
+import logo from "../assets/Logo1.png";
+import { Navigate, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
 
 function Copyright(props) {
@@ -30,7 +29,7 @@ function Copyright(props) {
     >
       {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
-       vanlyfe
+        vanlyfe
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
@@ -38,12 +37,9 @@ function Copyright(props) {
   );
 }
 
-
-
-const theme = createTheme();
+// const theme = createTheme();
 
 export default function Login() {
-
   const navigate = useNavigate();
   const [errors, setErrors] = useState({});
   const [form, setForm] = useState({
@@ -71,7 +67,6 @@ export default function Login() {
 
     setForm((f) => ({ ...f, [event.target.name]: event.target.value }));
   };
-  
 
   const handleOnSubmit = async (e) => {
     e.preventDefault();
@@ -85,111 +80,96 @@ export default function Login() {
     if (error) {
       setIsLoading(false);
       setErrors((e) => ({ ...e, form: error }));
-      
     }
 
-   
     if (data?.user) {
-
       setIsLoading(false);
       navigate("/listings");
       apiClient.setToken(data.token);
-      setIsLoading(false)
+      setIsLoading(false);
     }
   };
 
-
-
-
   return (
     <div className="login">
-    <Box>
-     
-        <ThemeProvider theme={theme}>
-          <Container  component="main" maxWidth="xs">
-            <CssBaseline />
-            <Box
-              sx={{
-                marginTop: 8,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-                <img src = {logo} />
-              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                <LockOutlinedIcon />
-              </Avatar>
-              <Typography component="h1" variant="h5">
-                Sign in
-              </Typography>
-              {errors.form && <span className="error">{errors.form}</span>}
-              <Box
-                component="form"
-               
-                noValidate
-                sx={{ mt: 1 }}
+      <Box>
+        {/* <ThemeProvider theme={theme}> */}
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <img src={logo} />
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            {errors.form && <span className="error">{errors.form}</span>}
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                onChange={handleOnInputChange}
+                autoComplete="email"
+                autoFocus
+              />
+              {errors.email && <span className="error">{errors.email}</span>}
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                onChange={handleOnInputChange}
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              {errors.password && (
+                <span className="error">{errors.password}</span>
+              )}
+              <br />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                onClick={handleOnSubmit}
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
               >
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  onChange={handleOnInputChange}
-                  autoComplete="email"
-                  autoFocus
-                />
-                {errors.email && <span className="error">{errors.email}</span>}
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  onChange={handleOnInputChange}
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-                {errors.password && (
-              <span className="error">{errors.password}</span>
-            )}
-            <br/>
-                <FormControlLabel
-                  control={<Checkbox value="remember" color="primary" />}
-                  label="Remember me"
-                />
-                <Button
-                 
-                  onClick={handleOnSubmit}
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                   {isLoading ? "Loading...." : "Sign In"}
-                </Button>
-                <Grid container>
-                  <Grid item xs>
-                    <a href="/forgot-password" variant="body2">
-                      Forgot password?
-                    </a>
-                  </Grid>
-                  <Grid item>
-                    <a href="/register" variant="body2">
-                      {"Don't have an account? Sign Up"}
-                    </a>
-                  </Grid>
+                {isLoading ? "Loading...." : "Sign In"}
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <a href="/forgot-password" variant="body2">
+                    Forgot password?
+                  </a>
                 </Grid>
-              </Box>
+                <Grid item>
+                  <a href="/register" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </a>
+                </Grid>
+              </Grid>
             </Box>
-            <Copyright sx={{ mt: 8, mb: 4 }} />
-          </Container>
-        </ThemeProvider>
-      
-    </Box>
+          </Box>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+        {/* </ThemeProvider> */}
+      </Box>
     </div>
   );
-
 }
