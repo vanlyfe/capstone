a715c8a527280b145b5f2db308efb8b942271584
Joram Bosire <joramb64@gmail.com>
2022-07-27 16:48:39 -0700

    Implement Rating GET requests

--- Backend/models/rating.js
+++ Backend/models/rating.js
@@ -3,6 +3,21 @@ const { BadRequestError } = require("../utils/errors");
 
 
 class Rating{
+    static async getRatingByListingId(listingId){
+        const result = await db.query(
+            `
+            SELECT AVG(rating)
+            FROM ratings
+            WHERE listing_id = $1
+            
+            `, [listingId]
+        )
+
+        const res = result.rows
+
+        return res
+    }
+
     
 }
 
