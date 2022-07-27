80a332afbdb482d9b6bead1133efd2dac1800d58
Ammar Fakih <ammar.fakih02@gmail.com>
2022-07-27 15:38:58 -0700

    Updated mobile menu and item spacing

--- Frontend/src/components/Listings.jsx
+++ Frontend/src/components/Listings.jsx
@@ -6,11 +6,14 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Paper, Slide } from '@mui/material';
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 
 import apiClient from '../services/apiClient';
 
 export default function Listings() {
+  const [mobileMenuOpen, setMobileMenuOpen] = React.useState(false);
   const [listings, setListings] = React.useState([]);
   const [error, setError] = React.useState(null);
 
@@ -28,33 +31,101 @@ export default function Listings() {
   }, []);
 
   return (
-    <Container maxWidth="100%" sx={{ mt: 0 }}>
+    <Container maxWidth="100%" sx={{ mt: 0, my: 0 }}>
       <Box container spacing={2} display="flex">
         {/* Desktop Filter List */}
-        <Box
+        <Paper
+          elevation={3}
           xs={2}
-          bgcolor="gray"
           sx={{
             mt: 5,
             mr: 3,
             height: '80vh',
             width: 240,
             display: { xs: 'none', md: 'block' },
-          }}></Box>
+          }}></Paper>
+
+        {/* Mobile Filter Bottom Bar */}
+
+        <Paper
+          elevation={3}
+          sx={{
+            zIndex: 1,
+            display: { xs: 'block', md: 'none' },
+            position: 'fixed',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: '7vh',
+            width: '100%',
+          }}>
+          <Box
+            sx={{
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+              mr: 3,
+            }}>
+            <Button
+              onClick={() => {
+                setMobileMenuOpen(true);
+              }}>
+              <Typography variant="h6">{`Filter`}</Typography>
+              <KeyboardDoubleArrowUpIcon />
+            </Button>
+          </Box>
+        </Paper>
+
         {/* Mobile Filter Menu */}
-        <Box>
-          
-        </Box>
+        <Slide direction={'up'} in={mobileMenuOpen} mountOnEnter>
+          <Paper
+            sx={{
+              zIndex: 1,
+              height: '80vh',
+              width: '100%',
+              position: 'fixed',
+              bottom: 0,
+              left: 0,
+              display: { xs: 'block', md: 'none' },
+            }}
+            elevation={3}>
+            <Box
+              sx={{
+                position: 'absolute',
+                top: 0,
+                height: '7vh',
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+                mr: 4,
+              }}>
+              <Button
+                onClick={() => {
+                  setMobileMenuOpen(false);
+                }}>
+                <Typography variant="h6">{`Filter`}</Typography>
+                <KeyboardDoubleArrowDownIcon />
+              </Button>
+            </Box>
+          </Paper>
+        </Slide>
+
         <Grid
           container
-          xs={10}
           // bgcolor="red"
-          sx={{ mt: 5 }}
+          sx={{ width: '100%', height: '100%' }}
           columns={{ xs: 4, sm: 8, md: 12 }}
           spacing={2}>
-          {listings.map((listing) => (
-            <Grid item xs={4}>
-              <Card sx={{ maxWidth: 345 }}>
+          <Grid item xs={12}>
+            <Typography variant="h5" sx={{ my: 2 }}>
+              {`Browse the Current Listings`}
+            </Typography>
+          </Grid>
+          {listings.map((listing, i) => (
+            <Grid key={i} item xs={4} justifyContent="center">
+              <Card sx={{ width: "100%" }}>
                 <CardMedia
                   component="img"
                   height="140"
