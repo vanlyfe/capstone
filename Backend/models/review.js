df9e8f6da0955792576b06976a3962baa2e6bdcc
Joram Bosire <joramb64@gmail.com>
2022-07-27 16:56:53 -0700

    Implemented review GET requests

--- Backend/models/review.js
+++ Backend/models/review.js
@@ -3,6 +3,20 @@ const { BadRequestError } = require("../utils/errors");
 
 
 class Review{
+
+    static async getReviewsByListingId(listingId){
+        const result = await db.query(
+            `
+            SELECT * 
+            FROM reviews
+            WHERE listing_id = $1
+            `, [listingId]
+        )
+
+        const res = result.rows
+        
+        return res
+    }
     
 }
 
