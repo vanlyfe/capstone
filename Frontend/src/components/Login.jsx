4db75d69ded9e3a58c51daf3f72a0f3acccbf31d
Joram Bosire <joramb64@gmail.com>
2022-07-29 11:07:27 -0700

    Deleted unnecessary import in login

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -5,7 +5,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Linker from "@mui/material/Link";
 import {Link} from "react-router-dom"
 
 
